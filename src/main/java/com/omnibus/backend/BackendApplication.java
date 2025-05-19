f4b0eb06b3d9b13c980e15cbdf978840e3977f67
NicoGithub758 <115185710+NicoGithub758@users.noreply.github.com>
2025-05-19 16:48:26 -0300

    Update BackendApplication.java

--- src/main/java/com/omnibus/backend/BackendApplication.java
+++ src/main/java/com/omnibus/backend/BackendApplication.java
@@ -8,3 +8,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
+
+

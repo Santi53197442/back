b4527cf91c72ef688ceedb45156076004f9fbf11
Santi53197442 <santipereyra64@gmail.com>
2025-05-23 12:40:21 -0300

    probando recuperar contraseña

--- src/main/java/com/omnibus/backend/config/SecurityConfig.java
+++ src/main/java/com/omnibus/backend/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "https://frontend-eosin-eight-41.vercel.app",
                 "https://frontend-pjqhx2c7u-santi53197442s-projects.vercel.app",
-                "https://frontend-git-master-santi53197442s-projects.vercel.app"
+                "https://frontend-git-master-santi53197442s-projects.vercel.app",
+                "https://frontend-4f1xmt5az-santi53197442s-projects.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*")); // O especifica los headers que necesitas
